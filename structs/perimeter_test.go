da1e9604a698195a850c8f8fe018f5057c3e2152
ProkhorovKV <ProkhorovKV@Piteravto.ru>
2024-04-26 20:52:23 +0300

    add table driven tests

--- structs/perimeter_test.go
+++ structs/perimeter_test.go
@@ -6,30 +6,26 @@ func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{Width: 10.0, Height: 10.0}
 	got := Perimeter(rectangle)
 	want := 40.0
-	assertEquals(t, want, got)
+	if got != want {
+		t.Errorf("got %g want %g", got, want)
+	}
 }
 
 func TestArea(t *testing.T) {
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{Width: 12.0, Height: 6.0}
-		got := rectangle.Area()
-		want := 72.0
-		assertEquals(t, want, got)
-	})
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{Width: 12.0, Height: 6.0}, 72.0},
+		{Circle{10.0}, 314.1592653589793},
+	}
 
-	t.Run("Circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.1592653589793
-		assertEquals(t, want, got)
-	})
-
-}
-
-func assertEquals(t *testing.T, want float64, got float64) {
-	t.Helper()
-	if got != want {
-		t.Errorf("want %g but got %g", want, got)
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		want := tt.want
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
 }
